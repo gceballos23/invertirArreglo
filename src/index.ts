11ca0d6f24bc14041563b181c811b369737bd0df
gceballos23 <103612856+gceballos23@users.noreply.github.com>
2022-05-29 16:54:34 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+let nro: number = 0;
+
+nro = Number(
+  prompt(
+    "Indique  número que desea incorporar en la posición ${indice} Para el Vector 1: "
+  )
+);
+
+let numerosIngresados: number[] = new Array(nro);
