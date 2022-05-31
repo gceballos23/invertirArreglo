80f0afdadd4f00d9e8b8bf68f5530c29aad11b88
gceballos23 <103612856+gceballos23@users.noreply.github.com>
2022-05-30 21:30:43 -0300

    invertir vector

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,43 @@ nro = Number(
 );
 
 let numerosIngresados: number[] = new Array(nro);
+
+function cargarVector(v: number[], cantidad: number) {
+  let indice: number;
+  for (indice = 0; indice < cantidad; indice++) {
+    v[indice] = Number(prompt("Ingrese el valor de la posicion", indice, ":"));
+  }
+}
+
+function invertirVector(v: number[], cantidad: number) {
+  let indice: number;
+  let indiceInvertido: number = 0;
+
+  let arrayInvertido: number[] = new Array(cantidad);
+  for (
+    indiceInvertido = cantidad - 1;
+    indiceInvertido > -1;
+    indiceInvertido--
+  ) {
+    arrayInvertido[indice] = v[indiceInvertido];
+    indice++;
+  }
+  console.log("Nro:", arrayInvertido);
+}
+
+function mostrarVertorInverso(v: number[], cantidad: number) {
+  cantidad--;
+  for (let indice = cantidad; indice > -1; indice--) {
+    console.log("Nro :", v[indice]);
+  }
+}
+
+function mostrarVertor(v: number[], cantidad: number) {
+  for (let indice = 0; indice < cantidad; indice++) {
+    console.log("Nro :", v[indice]);
+  }
+}
+
+cargarVector(numerosIngresados, nro);
+invertirVector(numerosIngresados, nro);
+//mostrarVertor(numerosIngresados, numerosIngresados.length);
